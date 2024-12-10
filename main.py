dc073b8a3ecbbecd3840bbf491bce7921993c5b3
ChenYao <chenyaoliew@gmail.com>
2024-12-11 02:58:14 +0800

    Login prompt complete

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def check_account_credentials(username, password):
 
     return found
 
-
 if check_account_credentials(input_username, input_password):
     print("Welcome, " + input_username + "!, Account Type: " + user_account_type)
 else:
