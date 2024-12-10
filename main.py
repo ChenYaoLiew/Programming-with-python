6ed3423fe817dfa634a124fe61cdb154c9b172d6
ChenYao <chenyaoliew@gmail.com>
2024-12-11 01:06:39 +0800

    Added user.py and user_data.txt for registering

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-accounts = open("data/user_data.txt", "r")
+with open("data/user_data.txt", "r") as file:
+    accounts = eval(file.read())
 
-print(accounts.read())
+input_username = input("Enter your username:")
+input_password = input("Enter your password:")
 
-# input_username = input("Enter your username:")
-# input_password = input("Enter your password:")
-#
-# def check_account_credentials(username, password):
-#     found = False
-#
-#     for key in accounts:
-#         data_username, data_password = accounts.strip().split('||')
-#
-#         if username == data_username and password == data_password:
-#             found = True
-#
-#     return found
-#
-# if check_account_credentials(input_username, input_password):
-#     print("Welcome, " + input_username + "!")
+def check_account_credentials(username, password):
+    found = False
+
+    for key in accounts:
+        if username == key["username"] and password == key["password"]:
+            found = True
+
+    return found
+
+if check_account_credentials(input_username, input_password):
+    print("Welcome, " + input_username + "!")
+else:
+    print("Login failed, please try again.")
