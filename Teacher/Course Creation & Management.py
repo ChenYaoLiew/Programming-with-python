2dfe22ccd0a3346a4ad1cdb0ddb93d2faf214701
Manfred <manfredllx@gmail.com>
2025-01-23 22:34:08 +0800

    course schedule finish

--- Teacher/Course Creation & Management.py
+++ Teacher/Course Creation & Management.py
@@ -3,8 +3,16 @@ def create_course():
 
     # Get course details of course name , code and schedule
     course_data["name"] = input("Create a course name: ")
-    course_data["code"] = int(input("Enter course code: "))
-    course_data["schedule"] = input("Enter course schedule: ")
+    course_data["code"] = input("Enter course code: ")
+
+    # Get Schedule Details
+    print("Creating Course Schedule: ")
+    course_data["schedule"] = []
+    while True:
+        entry = input("Enter a schedule entry or 'done' to finish (Example: Mon/Fri 9:00am-10:00am): ")
+        if entry.lower() == "done":
+            break
+        course_data["schedule"].append(entry)
 
     # Get lesson plan details
     course_data["lesson_plan"] = {} # Nested dictionary for lesson plan inside course_data dictionary
@@ -22,15 +30,15 @@ def create_course():
         with open(teach_file_path, "a") as f:
             f.write(f"Course Name: {course_data["name"]}\n")
             f.write(f"Course Code: {course_data["code"]}\n")
-            f.write(f"Schedule: {course_data["schedule"]}\n")
+            f.write(f"Course Schedule: {course_data["schedule"]}\n")
 
             f.write(f"Lesson Plan: {course_data["lesson_plan"]["title"]}\n")
-            f.write(f" Lesson description: {course_data["lesson_plan"]["description"]}\n")
+            f.write(f"Lesson description: {course_data["lesson_plan"]["description"]}\n")
 
             f.write(f"Assignment: {course_data["Assignment"]["title"]}\n")
             f.write(f"Assignment Description: {course_data["Assignment"]["description"]}\n")
 
-            f.write("---/n")
+            f.write("----------\n")
 
     except FileNotFoundError:
         print("Error creating course file ")
