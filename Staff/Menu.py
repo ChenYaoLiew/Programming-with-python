71f5752d3eb621b0f540b8fbe86ea07dda3f25b9
Burger0205 <khorelson@gmail.com>
2025-03-09 19:45:47 +0800

    update staff menu feature
    - letting staff menu can access view timetable

--- Staff/Menu.py
+++ Staff/Menu.py
@@ -1,13 +1,18 @@
+import sys
+import os
+
+# Add the project root directory to Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 def staff_user_page():
     while True:
         print("\nStaff Menu")
         print("'1' - Student Record")
         print("'2' - Resource Management")
-        print("'3' - Timetable Management")
+        print("'3' - View Course Timetable")
         print("'4' - Event Management")
         print("'5' - Communication ")
         print("'6' - Exit")
-        print("'7' - Logout")
 
         choice = input("Enter your choice: ")
         if choice == '1':
@@ -17,18 +22,15 @@ def staff_user_page():
             from Staff.Resource_Allocation import manage_resource_allocation_main
             manage_resource_allocation_main()
         elif choice == '3':
-            pass
-            # from Staff.Timetable_Management import timetable_management_main
-            # timetable_management_main()
+            from Administrator.course_management import view_course_timetable
+            view_course_timetable()
         elif choice == '4':
             from Staff.Event_Management import event_management_main
             event_management_main()
         elif choice == '5':
             from Staff.Communication import communication_main
-            communication_main
+            communication_main()
         elif choice == '6':
-            quit()
-        elif choice == '7':
             quit()
         else:
             print("Invalid choice!")
